1732e7d8b73f0ced0397718f06a8ffbfc4e4d954
Siza <laurenadhikari16@gmail.com>
2023-06-22 16:29:12 +0545

    new updates

--- employee/fourthhigh.sql
+++ employee/fourthhigh.sql
@@ -1,6 +1,5 @@
 SELECT *
 FROM(
 SELECT salary, employee, id, dense_rank() 
-OVER (ORDER BY salary ASC)fourthhigh FROM employee) AS high
-WHERE fourthhigh=4
-OR fourthhigh=3;
+OVER (ORDER BY salary DESC)fourthhigh FROM employee) AS high
+WHERE fourthhigh=4;
